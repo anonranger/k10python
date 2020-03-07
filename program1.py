dd291dd74156ed1d1ff1c96676a97bccd81c7860
anonranger <anonranger@protonmail.com>
2020-03-07 08:44:59 -0800

    first program

--- program1.py
+++ program1.py
@@ -1,22 +1,22 @@
-class Dog:
+class dog:
     # create cups/initialize class constructor
-    def __init__(self, name, temperature):
+    def __init__(self, name, temp):
         self.name = name
-        self.temperature = temperature
+        self.temp = temp
 
-    # create function to check temperature
+    # create function to check temp
     def check_temp(self):
-        if self.temperature > 99:
+        if self.temp > 99:
             print(self.name, "has high fever.")
         else:
             print(self.name, "has no fever.")
 
-# create and assign object variables
-dog1_name = 'cupcake'
-dog1_temp = 100
+# create and add object variables
+dog1_name = 'lucky'
+dog1_temp = 190
 
 # order cups / create object
-object1_dog = Dog(dog1_name, dog1_temp)
+object1 = dog(dog1_name, dog1_temp)
 
-# call function check temperature
-object1_dog.check_temp()
+# call function check temp
+object1.check_temp()
